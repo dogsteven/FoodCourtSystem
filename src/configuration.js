ff1ff1b08e60cf300ce71fbcb8c4ce17a3f0c9dc
dogsteven <caigicungduoc23@gmail.com>
2020-06-18 15:36:50 +0700

    fix configuration

--- src/configuration.js
+++ src/configuration.js
@@ -1,5 +1,5 @@
-{
-    "database": {
+export default {
+    database: {
         "customer": "/FoodCourt/Customer",
         "food-item": "/FoodCourt/FoodItem",
         "order": "/FoodCourt/Order",
